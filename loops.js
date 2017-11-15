918a4a7b58dc8fcafde7c98883311019d1a45d67
Bushra Taimoor <bushra376@hotmail.com>
2017-11-15 21:24:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,8 @@ function forLoop(arr){
   for(let i=0; i<25; i++){
     if (i === 1){
       arr[i] = "I am 1 strange loop.";
-    }else{
+    }
+    else{
       arr[i] = `I am ${i} strange loops.`;
     }
   }
